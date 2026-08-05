84660121fc109e87e4abe865e527ecc3bbbe68e0
thebasu10-lab <thebasu10@gmail.com>
2026-08-05 21:08:19 +0530

    Add type modifiers example in C++
    In C++, type modifiers are keywords used to alter the storage size, value range, or sign of fundamental data types (int, char, and double). They allow developers to optimize memory and adjust precision to suit specific application needs without creating entirely new types.

---
+++ Day01_Basic_Syntax_Data_Types/04_type_modifiers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    short int s = 32000;
+    unsigned int u = 4000000000;
+    long int l = 1234567890L;
+    long long int ll = 9223372036854775807LL;
+    unsigned long long ull = 18446744073709551615ULL;
+
+    cout << "short int: " << s << endl;
+    cout << "unsigned int: " << u << endl;
+    cout << "long int: " << l << endl;
+    cout << "long long int: " << ll << endl;
+    cout << "unsigned long long: " << ull << endl;
+
+    return 0;
+}
+//output
+//short int: 32000
+//unsigned int: 4000000000
+//long int: 1234567890
+//long long int: 9223372036854775807
+//unsigned long long: 18446744073709551615
