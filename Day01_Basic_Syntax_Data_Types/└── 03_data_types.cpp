5afeab17d783c7150c052ffe4a9349e343f4d4d3
thebasu10-lab <thebasu10@gmail.com>
2026-08-05 19:52:53 +0530

    Day 1: 03 Data types
    This file contains my Day 1 C++ practice on data types. I learned how different data types such as int, float, double, char, bool, and string are used to store different kinds of values. I also practiced declaring variables and displaying their values using cout.

---
+++ Day01_Basic_Syntax_Data_Types/└── 03_data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int age = 20;
+    float temperature = 36.5f;
+    double price = 99.99;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Adarsh";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Temperature: " << temperature << endl;
+    cout << "Price: " << price << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
