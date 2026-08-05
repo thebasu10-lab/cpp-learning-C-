2653587c745f8236a7aa367f1890bc9dbd12f61f
thebasu10-lab <thebasu10@gmail.com>
2026-08-06 00:28:01 +0530

    Day 3: 01_if statement
    This file contains my Day 3:01_C++ practice on the if statement. I learned how an if statement is used to execute a block of code only when a given condition is true.

---
+++ Day03_Control_Statements/01_if_statement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//if (condition) {
+    // statements
+}
+
+int main() {
+    int age = 20;
+
+    if (age >= 18) {
+        cout << "You are eligible to vote." << endl;
+    }
+
+    cout << "Program continues..." << endl;
+    return 0;
+}
