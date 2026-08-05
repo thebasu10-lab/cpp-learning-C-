69fcdac1442c227fb980d8a6156e691cbbb7aeb6
thebasu10-lab <thebasu10@gmail.com>
2026-08-06 00:36:57 +0530

    Day 3: 06_while loop
    This file contains my Day 3:06_ C++ practice on the while loop. I learned how a while loop repeatedly executes a block of code as long as its condition remains true.

---
+++ Day03_Control_Statements/└──06_while_loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+//while (condition) {
+    // body
+}
+
+int main() {
+    int i = 1;
+    while (i <= 5) {
+        cout << i << " ";
+        ++i;
+    }
+    cout << endl;
+
+    // Read until user enters 0
+    int num;
+    cout << "Enter numbers (0 to stop): ";
+    cin >> num;
+    while (num != 0) {
+        cout << "You entered: " << num << endl;
+        cin >> num;
+    }
+    return 0;
+}
