935e3fb83f5065bf9fabff5053720e74f800ddef
thebasu10-lab <thebasu10@gmail.com>
2026-08-06 00:29:35 +0530

    Day 3: 02_f-else statement
    This file contains my Day 3:02_ C++ practice on the if-else statement. I learned how if-else allows a program to choose between two different blocks of code depending on whether a condition is true or false.

---
+++ Day03_Control_Statements/└──02_if_else_statement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//if (condition) {
+    // true path
+} //else {
+    // false path
+}
+
+int main() {
+    int number = 7;
+
+    if (number % 2 == 0) {
+        cout << number << " is even." << endl;
+    } else {
+        cout << number << " is odd." << endl;
+    }
+    return 0;
+}
