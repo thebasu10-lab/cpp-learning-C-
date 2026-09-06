d14c42fb1bf346bfebbc87a15ce6912bbc25c9f4
thebasu10-lab <thebasu10@gmail.com>
2026-09-06 23:22:20 +0530

    Implement inverted triangle pattern with nested loops
    Added code to print an inverted triangle pattern using nested loops.

--- Day03_Control_Statements/└──08_nested_loops.cpp
+++ Day03_Control_Statements/└──08_nested_loops.cpp
@@ -7,6 +7,13 @@ int main() {
             cout << i * j << "\t";
         }
         cout << endl;
+        cout << "\n3. Inverted triangle:" << endl;
+    for (int i = 5; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
     }
     return 0;
 }
